e288654699c50406ce895c47ed6f92c8869d7c66
Erik Howard <ribs@Eriks-MacBook-Pro.local>
2017-02-28 18:17:13 -0600

    Initial Commit

---
+++ xCode8GitExample/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  xCode8GitExample
+//
+//  Created by Erik Howard on 2/28/17.
+//  Copyright © 2017 Erik Howard. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ xCode8GitExample/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  xCode8GitExample
+//
+//  Created by Erik Howard on 2/28/17.
+//  Copyright © 2017 Erik Howard. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
